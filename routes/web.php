2a19f15dbb9540977d4dc7c8c6665075f072198f
haiou2001 <2111255898@qq.com>
2020-03-26 13:33:48 +0800

    阅读

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('live/index','live\IndexController@index');//��½��ͼ
+
+
+Route::get('live/reg','live\LiveController@reg');//ע����ͼ
+Route::post('live/reg_do','live\LiveController@reg_do');//ע��
+Route::get('live/login','live\LiveController@login');//��½��ͼ
+Route::post('live/login_do','live\LiveController@login_do');//��½�阿萨德
